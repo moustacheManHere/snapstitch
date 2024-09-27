3c9e07d0b47d67a21df9fb19ba5aab8c3e217573
ram <justforthegreatergood@gmail.com>
2024-09-27 08:55:48 +0800

    Added code to randomly select coordinates to place parts

--- src/snapstitch/generator.py
+++ src/snapstitch/generator.py
@@ -1,5 +1,9 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 import numpy as np
+import random
+
+# Types for type hinting [x,y]
+Coordinates = Tuple[int, int]
 
 
 # Generic Class for all generators
@@ -33,21 +37,53 @@ class YOLOv8Generator(Generator):
         label_dir: str,
         image_name: str,
     ) -> List[np.ndarray]:
+
+        # Loop through all the parts
+        # Get a random position for each
+        # Place the part on the background
+        # Save the labels
         pass
 
     def _get_new_part_position(
-        current_positions: List[Tuple[int, int]]
-    ) -> Tuple[int, int]:
-        pass
+        current_positions: List[Coordinates],
+        part_size: Coordinates,
+        background_size: Coordinates,
+    ) -> Optional[Coordinates]:
+        max_attempts = 10
+
+        # try to find a random position that doesn't overlap
+        for i in range(max_attempts):
+            # x,y refers to the top left coordinate of the part, not center
+            random_x = random.randint(0, background_size[0] - part_size[0])
+            random_y = random.randint(0, background_size[1] - part_size[1])
+
+            # Check if the new part overlaps with any existing parts
+            overlap = False
+            for current_position in current_positions:
+                # TODO: Check if the overlap ratio is within the threshold
+                if (
+                    random_x < current_position[0] + part_size[0]
+                    and random_x + part_size[0] > current_position[0]
+                    and random_y < current_position[1] + part_size[1]
+                    and random_y + part_size[1] > current_position[1]
+                ):
+                    overlap = True
+                    break
+
+            if overlap:
+                continue
+
+            return random_x, random_y
+
+        return None
 
     def _place_part(
-        background: np.ndarray, part: np.ndarray, position: Tuple[int, int]
+        background: np.ndarray, part: np.ndarray, position: Coordinates
     ) -> np.ndarray:
         pass
 
-    def _save_labels(
-        current_positions: List[Tuple[int, int]], output_path: str
-    ) -> bool:
+    def _save_labels(current_positions: List[Coordinates], output_path: str) -> bool:
+        # need to convert x1,y1,x2,y2 to x_center, y_center, width, height for YOLO
         pass
 
     def _save_image(image: np.ndarray, output_path: str) -> bool:
